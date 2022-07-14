88f421bf78503ab5e1603a38ccdec3abeb145634
Nishant Gada <nishant.gada@think360.ai>
2022-07-14 15:57:49 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+@app.route('/')
+def homepage():
+    return "Test"
+
+@app.route('/getFirstName', methods=['GET'])
+def getName():
+    name = {
+        "firstName": "Nishant"
+    }
+    result = jsonify(name)
+    return result
+
+@app.route('/getLastName', methods=['GET'])
+def getName():
+    name = {
+        "lastName": "Gada"
+    }
+    result = jsonify(name)
+    return result
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
