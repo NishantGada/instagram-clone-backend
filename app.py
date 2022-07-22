26b84b72a0a900e2ab527ff3e2065cd83a9f5e65
Nishant Gada <nishant.gada@think360.ai>
2022-07-22 13:31:26 +0530

    Created saveUserDetails endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
@@ -15,26 +16,38 @@ def getFirstName():
     result = jsonify(name)
     return result
 
-@app.route('/getLastName', methods=['GET'])
-def getLastName():
-    name = {
-        "lastName": "Gada"
-    }
-    result = jsonify(name)
-    return result
+@app.route('/saveUserDetails', methods=['POST'])
+def saveUserDetails():
+
+    if request.method == 'POST':
+        return({
+            "status": "Success",
+            "description": "User details saved successfully",
+            "data": {
+                "name": request.json['name'],
+                "email": request.json['email'],
+                "phone": request.json['phone'],
+                "bio": request.json['bio'],
+                "number_of_posts": request.json['number_of_posts'],
+                "posts": request.json['posts'],
+                "followers": request.json['followers'],
+                "following": request.json['following'],
+            }
+        })
+
 
 @app.route('/getUserDetails', methods=['GET'])
 def getUserDetails():
-    return({
-        "name": "Nishant Gada",
-        "email": "test@gmail.com",
-        "phone": "9022379777",
-        "bio": "Namaste",
-        "number_of_posts": "10",
-        "posts": [],
-        "followers": "10,000",
-        "following": "250",
-    })
+        return({
+            "name": "Nishant Gada",
+            "email": "test@gmail.com",
+            "phone": "9022379777",
+            "bio": "Namaste",
+            "number_of_posts": "10",
+            "posts": [],
+            "followers": "10,000",
+            "following": "250",
+        })
     
 
 if __name__ == '__main__':
